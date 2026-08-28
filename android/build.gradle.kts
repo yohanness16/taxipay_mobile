b808f24e0e6c8804dbdd91b8b0eb95d0ca1cb340
yohanness16 <yohanness1621@gmail.com>
2026-08-28 17:32:30 +0300

    Add automatic namespace fallback for legacy Android plugins in Gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,25 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (android != null && android.namespace.isNullOrEmpty()) {
+            val manifest = file("src/main/AndroidManifest.xml")
+            if (manifest.exists()) {
+                val match = Regex("""package\s*=\s*"([^"]+)"""").find(manifest.readText())
+                if (match != null) {
+                    android.namespace = match.groupValues[1]
+                } else {
+                    android.namespace = "com.plugin.${project.name.replace('-', '_')}"
+                }
+            } else {
+                android.namespace = "com.plugin.${project.name.replace('-', '_')}"
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
