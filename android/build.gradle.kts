60a6dec9f9cf01a549df862a8518f8da55bc6f86
yohanness16 <yohanness1621@gmail.com>
2026-08-28 17:51:56 +0300

    Enforce Java 17 and Kotlin JVM 17 compatibility across all Gradle subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,19 +22,36 @@ subprojects {
 subprojects {
     plugins.withId("com.android.library") {
         val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
-        if (android != null && android.namespace.isNullOrEmpty()) {
-            val manifest = file("src/main/AndroidManifest.xml")
-            if (manifest.exists()) {
-                val match = Regex("""package\s*=\s*"([^"]+)"""").find(manifest.readText())
-                if (match != null) {
-                    android.namespace = match.groupValues[1]
+        if (android != null) {
+            if (android.namespace.isNullOrEmpty()) {
+                val manifest = file("src/main/AndroidManifest.xml")
+                if (manifest.exists()) {
+                    val match = Regex("""package\s*=\s*"([^"]+)"""").find(manifest.readText())
+                    if (match != null) {
+                        android.namespace = match.groupValues[1]
+                    } else {
+                        android.namespace = "com.plugin.${project.name.replace('-', '_')}"
+                    }
                 } else {
                     android.namespace = "com.plugin.${project.name.replace('-', '_')}"
                 }
-            } else {
-                android.namespace = "com.plugin.${project.name.replace('-', '_')}"
+            }
+            android.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
             }
         }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
     }
 }
 
